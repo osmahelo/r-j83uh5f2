31e067da870424bff77a33637f2036cad1aed4ab
Oscar <omherreral81@gmail.com>
2021-12-13 04:57:11 -0500

    resuelto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.Contador = this.Contador.bind(this);
+
+    this.state ={
+      arr:""
+    }
+
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" value={this.state.arr} onChange={this.Contador}></textarea>
+        <div className="counter">{this.state.arr.length}</div>
       </div>
     );
+  }
+  Contador(e){
+    this.setState({
+      arr: e.target.value
+    })
+
   }
 }
 
